a8690c31ccdf4f8ae6a222f4d00a7ae600ad31eb
Elsayed Mostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2021-06-10 14:07:14 +0200

    added time statistics calculations

--- sender2.py
+++ sender2.py
@@ -1,25 +1,27 @@
 from socket import *
+import sys
 from splitter_function_demo import get_packets
 from threading import Timer
 import random
-eps = 1
+import time
 
-timers = [] # list of timers
-file_name = "apply.txt"
-packets = get_packets(file_name, mss=20)  # get packets
+eps = 1
+file_name = sys.argv[1]
+packets = get_packets(file_name, mss = 15)  # get packets
 n_packets = len(packets)  # number of packets to be transmitted
 
 # CLIENT parameters
 # port number and host name
-server_port = 12500
-host_name = "192.168.1.3"
+server_port = int(sys.argv[3])
+host_name = sys.argv[2]
+
 
 # create client socket
 client_socket = socket(AF_INET, SOCK_DGRAM)
 client_socket.settimeout(0.001)
 
 # set time out
-timeout = 1     # in second
+timeout = 0.5     # in seconds
 
 # Window parameters
 N = 3  # window size
@@ -37,7 +39,6 @@ def timeout_event(index):
 
 def check_and_retransmit():
     global timeout_flag, window_base, sent_pkt_index, timer
-    print('here..')
     # Resend the packets sent after it
     if random.randint(1, 10) > eps:
         client_socket.sendto(packets[window_base].encode(), (host_name, server_port))
@@ -60,6 +61,10 @@ while sent_pkt_index < n_packets - 1 or window_base < n_packets:
     # while not all packets are transmitted and empty window size is available
     while Remaining_N > 0 and sent_pkt_index < n_packets - 1:
         # index of pkt to be transmitted next
+        # CHECK if the sent_pkt_index == -1? START TIMER
+        if sent_pkt_index == -1:
+            start_time = time.time()
+
         sent_pkt_index += 1
         # transmit pkt
         if random.randint(0, 10) > eps:
@@ -94,5 +99,9 @@ while sent_pkt_index < n_packets - 1 or window_base < n_packets:
     except:  # nothing to do
         pass
 
+finish_time = time.time()
+print('Completely transmitted/received')
+# Compute statistics
 
-
+print('elapsed time = {:.3f}'.format(finish_time-start_time))
+print('Transmission rate = {:.3f} packet/second'.format(n_packets/(finish_time-start_time)))
